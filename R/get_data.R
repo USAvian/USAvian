9a69f75a08c317ba9876669a81334fda95ad9435
trashbirdecology <jessicaleighburnett@gmail.com>
2020-01-14 17:15:45 -0700

    Update get_data.R
    a function for importing files into memory from hetergeneous extensions/sources.
    
    called in import_data

--- R/get_data.R
+++ R/get_data.R
@@ -1,4 +1,5 @@
 #' @description An internal function for getting various filetypes into memory. Called from inside `import_data`
+#' @title Retrieve multiple filetypes from disk.
 #' @param
 #' @param
 #' @param
